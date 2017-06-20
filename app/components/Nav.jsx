ac05ce94694d4d01e7c6e08c3d58804243a48a77
Kai Barnes <barnes.kai@gmail.com>
2017-06-20 09:34:37 +0900

    Add navigation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,24 +1,27 @@
 var React = require("react");
 var {Link, IndexLink} = require("react-router");
 
-var Nav = (props) => {
+var Nav = () => {
   return (
     <div className="top-bar">
       <div className="top-bar-left">
-      <ul className="menu">
-        <li className="menu-text">React Timer App</li>
-        <li>
-          <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: "bold"}}>Timer</IndexLink>
-        </li>
-        <li>
-          <Link to="/" activeClassName="active-link" activeStyle={{fontWeight: "bold"}}>Countdown</Link>
-        </li>
-      </ul>
-
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
       </div>
       <div className="top-bar-right">
         <ul className="menu">
-          <div className="menu-text">Created by <a href="https://github.com/kaibarnes" target="_blank">Kai Barnes</a></div>
+          <div className="menu-text">
+            Created by <a href="https://github.com/kaibarnes" target="_blank">Kai Barnes</a>
+          </div>
         </ul>
       </div>
     </div>
